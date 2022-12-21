d261d30fbfbf27f1a7d1402a77284554db5276cf
Griffin <ffirgn@gmail.com>
2022-12-20 17:16:14 -0800

    product-tags created per homework instructions

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,21 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    // Defining columns based on requirements from the homework instructions.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: { model: 'product',key: 'id' },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: { model: 'tag', key: 'id' }
+    }
   },
   {
     sequelize,
